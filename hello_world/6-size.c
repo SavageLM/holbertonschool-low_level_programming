dd0473b51defced992e0852f5c1a1cdd1ab71e11
SavageLM <nagol.egavas@gmail.com>
2023-05-24 19:03:57 -0700

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	printf("Size of a char is %2d byte(s)\n", sizeof(char));
-	printf("Size of a int is %2d byte(s)\n", sizeof(int));
-	printf("Size of a long int is %2d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int is %2d byte(s)\n", sizeof(long long int));
-	printf("Size of a float is %2d byte(s)\n", sizeof(float));
+	printf("Size of a char %2d byte(s)\n", sizeof(char));
+	printf("Size of an int %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float %2d byte(s)\n", sizeof(float));
 
 	return (0);
 }
