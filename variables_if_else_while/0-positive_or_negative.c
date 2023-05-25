7eb86e80fc13b45383835b54983bbd656e5be2e0
SavageLM <nagol.egavas@gmail.com>
2023-05-25 10:18:52 -0700

    Task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,17 +17,17 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d is positive", n);
 	}
 
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero", n);
 	}
 
 	else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative", n);
 	}
 
 	return (0);
