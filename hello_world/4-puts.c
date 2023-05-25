9d74a2b9e7d136fddc73330d19c3f52a0eac9fb9
SavageLM <nagol.egavas@gmail.com>
2023-05-24 17:59:54 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return 0;
+	/*Prints out specified string.*/
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
