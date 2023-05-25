9cc1991c9bcffac6dd494e6bea9ea1d0337578e2
SavageLM <nagol.egavas@gmail.com>
2023-05-25 11:09:49 -0700

    Task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	char alpha;
 	
-	for (alpha = 'a'; alpha <= 'z' ; alpha++);
-	{
-		putchar(alpha\n);
-	}
+		for (alpha = 'a'; alpha <= 'z' ; alpha++);
+		{
+			putchar(alpha\n);
+		}
 
 	return (0);
 }
