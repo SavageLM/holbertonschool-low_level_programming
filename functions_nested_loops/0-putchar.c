1aefa3a39b7b77981cf024e0abc9b3a52179ae86
SavageLM <nagol.egavas@gmail.com>
2023-05-26 11:07:31 -0700

    Task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-        _putchar("_");
+	{
+	_putchar("_");
         _putchar("p");
         _putchar("u");
         _putchar("t");
@@ -18,6 +19,7 @@ int main(void)
         _putchar("a");
         _putchar("r");
 	_putchar("\n");
+	}
 
         return (0);
 }
