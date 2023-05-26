fde47bb6aa1258b9913b81223734b795fb0c21c6
SavageLM <nagol.egavas@gmail.com>
2023-05-26 09:56:46 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	{
+		char i = _;
+
+		_putchar(i);
+
+		i = p;
+
+		_putchar(i);
+
+		i = u;
+
+		_putchar(i);
+
+		i = t;
+
+		_putchar(i);
+
+		i = c;
+		_putchar(i);
+
+		i = h;
+		_putchar(i);
+
+		i = a;
+		_putchar(i);
+
+		i = r;
+
+		_putchar(i);
+	}
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+THis project will be about creating functions and nested loops. All files will contain code for functions and loops.
