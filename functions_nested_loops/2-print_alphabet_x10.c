271676b14096780e2d2d4aeb215c4fdb75b0bba8
SavageLM <nagol.egavas@gmail.com>
2023-05-26 12:09:14 -0700

    tASK 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+	int x = 1
+
+	while (x < 11; x++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	{
+		_putchar('\n');
+	}
+
+}
