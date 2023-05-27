a2a9fc3d38619dad756152bdfd8cde66727594ed
SavageLM <nagol.egavas@gmail.com>
2023-05-26 17:29:14 -0700

    Task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * Return: Return 0 if successful
+ * @c: input to check
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if ((c > 96) && (c < 123))
+	{
+		_putchar(49);
+	}
+	else
+	{
+		-putchar(48);
+	}
+
+	Return(0)
+
+}
