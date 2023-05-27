3a2723a6de5342756d8742baf1bb5d439c4b073f
SavageLM <nagol.egavas@gmail.com>
2023-05-26 18:46:25 -0700

    Task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@ int print_sign(int n)
 
 	if (n > 48)
 	{
+		_putchar(43)
 		return (1);
-		_putchar(43);
 	}
-	else if(n == 48)
+	else if (n == 48)
 	{
 		return (0);
 		_putchar(48);
 	}
-	else if(n < 48)
+	else if (n < 48)
 	{
 		return (-1);
 		_putchar(45);
