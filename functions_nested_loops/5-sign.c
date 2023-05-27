d5b73072c190e019a0cc08e4d1d944e53e3b9b3e
SavageLM <nagol.egavas@gmail.com>
2023-05-26 18:22:35 -0700

    Task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Return: Return 0 if successful
+ * @n: input to check
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 48)
+	{
+		return (1);
+		-putchar(43);
+	}
+	if else(n = 48)
+	{
+		return (0);
+		_putchar(48);
+	}
+	if else(n < 48)
+	{
+		return (-1);
+		_putchar(45)
+	}
+
+	return (0)
+
+}
