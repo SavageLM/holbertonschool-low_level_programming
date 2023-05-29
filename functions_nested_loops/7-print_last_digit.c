4bcc04f6d7a539085cbedcc9785bbc1a6520abb8
SavageLM <nagol.egavas@gmail.com>
2023-05-29 11:08:16 -0700

    Task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * Return: Return 0 if successful
+ * @n: input to check
+ */
+
+int print_last_digit(int n)
+{
+
+	x = n % 10;
+
+	Return(x);
+
+}
