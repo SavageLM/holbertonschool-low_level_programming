4735a1d61d5fab7f3d0007dbd0b022d683021bd8
SavageLM <nagol.egavas@gmail.com>
2023-05-29 10:16:21 -0700

    Task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Return: Return 0 if successful
+ * @n: input to check
+ */
+
+int _abs(int);
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+
+	return (0);
+
+}
