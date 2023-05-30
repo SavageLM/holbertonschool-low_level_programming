615690f648b784e4d5ee6db50e9d958a1a954299
SavageLM <nagol.egavas@gmail.com>
2023-05-29 20:50:09 -0700

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 			if (product <= 9)
 				_putchar(' ');
 			else
-				_putchar((product / 10) + '0')
+				_putchar((product / 10) + '0');
 
 			_putchar((product % 10) + '0');
 
