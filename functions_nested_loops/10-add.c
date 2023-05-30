6b231b5c4ab898a042304719212d0bc9b233e24b
SavageLM <nagol.egavas@gmail.com>
2023-05-29 21:12:02 -0700

    Task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @x: First number to be added
+ * @y: Second number to be added
+ *
+ * Return: Sum of x =+ y
+ */
+
+int add(int x, int y);
+{
+	return (x + y);
+}
