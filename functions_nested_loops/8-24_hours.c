d79d4f28424633da76abf6fa6dcf1082223cd3e8
SavageLM <nagol.egavas@gmail.com>
2023-05-29 19:59:27 -0700

    Task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Return: Return 0 if successful
+ *
+ */
+
+void jack_bauer(void)
+{
+
+	int hours, minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchare('\n');
+		}
+	}
+
+}
