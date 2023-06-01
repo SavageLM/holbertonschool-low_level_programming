350ed062055f581e09eed7f85ae7a6fc786e7838
SavageLM <nagol.egavas@gmail.com>
2023-06-01 09:28:43 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - entry point to function
+ * 
+ * 
+ * 
+ */
+
+void print_numbers(void)
+{
+	int i
+	
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
