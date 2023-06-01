15226cc5c69d6fd959637b0463ef5ddf2955b3c9
SavageLM <nagol.egavas@gmail.com>
2023-06-01 11:08:48 -0700

    Task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point to function
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int x; 
+
+	while(x < 11)
+		for (i = 0; i < 15; i++)
+		{
+			_putchar(i + '0');
+		}
+
+	_putchar('\n');
+	x++;
+}
