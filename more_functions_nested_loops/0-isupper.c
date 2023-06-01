8439549a4a5436cc8349c7c8d0d8f5c09cac703d
SavageLM <nagol.egavas@gmail.com>
2023-06-01 07:45:00 -0700

    Task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - entry point to function
+ * Return: returns 1 if uppercase, 0 if not
+ * @c: input to check
+ */
+
+int _isupper(int c)
+{
+	if ((c > 64) && (c < 91))
+		return (1)
+	else
+		return(0)
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This README is for the project on Pointer and functions.
