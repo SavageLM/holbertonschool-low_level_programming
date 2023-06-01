745606bd0b67991146c76e15b4d40c3c31a26ef2
SavageLM <nagol.egavas@gmail.com>
2023-06-01 08:35:13 -0700

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - entry point to function
+ * Return: returns the product of a and b
+ * @a: input to check
+ * @b: input to check
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
