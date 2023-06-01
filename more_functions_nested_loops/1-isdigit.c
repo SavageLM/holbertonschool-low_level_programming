d6d3979a9b114b108631d8895dd7f3492b738d72
SavageLM <nagol.egavas@gmail.com>
2023-06-01 07:56:00 -0700

    Task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - entry point to function
+ * Return: returns 1 if a digit, 0 if not
+ * @c: input to check
+ */
+
+int _isupper(int c)
+{
+	if ((c > 47) && (c < 58))
+		return (1);
+	else
+		return (0);
+}
