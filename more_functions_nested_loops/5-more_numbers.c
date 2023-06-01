c8ba7c04bc080f6ba84b14bcf8c4354bd7e1ebb0
SavageLM <nagol.egavas@gmail.com>
2023-06-01 11:51:25 -0700

    Task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,10 +16,13 @@ void more_numbers(void)
 	{
 		int i;
 
-		for (i = 0; i < 15; i++)
+		for (i = 0; i < 10; i++)
 		{
-			_putchar(i + '0');
+			if (i > 9)
+				_putchar((i / 10) + '0')
+			_putchar((i % 10) + '0');
 		}
+
 	}
 
 	_putchar('\n');
