97eaf0481f56f5217995a1922460274f52d1cca6
SavageLM <nagol.egavas@gmail.com>
2023-06-01 10:11:32 -0700

    Task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point to function
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	while (i = 0; i <= 9; i++)
+		if ((i != 2) || (i != 4)
+		{
+			_putchar(i + '0');
+		}
+
+	_putchar('\n');
+}
