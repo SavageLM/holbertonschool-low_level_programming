f6ffebc9b99db79310e7fc35be98e7fc77986643
SavageLM <nagol.egavas@gmail.com>
2023-06-01 18:42:47 -0700

    Task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - entry point to function
+ * @size: Number of times to print "#"
+ *
+ *
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+
+	else
+	{
+		int x = 1;
+
+		while (x <= n)
+		{
+			for (row = 1; row <= n; row++;)
+			{
+				_putchar('#');
+			}
+
+		_putchar('\n');
+		x++;
+		}
+	}
+}
