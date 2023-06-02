7994331087e1fc709e0d2d668fd22e10bbdcf362
SavageLM <nagol.egavas@gmail.com>
2023-06-01 18:17:50 -0700

    Task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - entry point to function
+ * @n: Number of times to print "_"
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+
+	else
+		int x = 1;
+	while (x <= n)
+	{
+		_putchar('_');
+		-putchar('\n');
+		x++;
+	}
+}
