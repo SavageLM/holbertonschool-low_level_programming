bae3effc991537744fd5cbf671315c9bc5d50369
SavageLM <nagol.egavas@gmail.com>
2023-06-02 08:53:30 -0700

    Task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - entry point to function
+ * @n: Number of \ characters
+ *
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space < line; space++)
+
+				_putchar(' ');
+
+			_putchar('\\');
+
+			if (line == n - 1)
+				break;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n'
+}
