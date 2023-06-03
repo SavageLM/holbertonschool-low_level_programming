f5aa31d5d9ebe38f0c13381e0682721425aebb7f
SavageLM <nagol.egavas@gmail.com>
2023-06-02 17:43:14 -0700

    Task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - entry to function
+ * @a: pointer to an int variable to be swapped
+ * @b: pointer to an int variable to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
