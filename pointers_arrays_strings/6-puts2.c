08c1f21fed3073fe9805088ec0bedccd97de3874
SavageLM <nagol.egavas@gmail.com>
2023-06-05 18:29:49 -0700

    Task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - entry to function
+ * @str: string to be printed
+ */
+
+void puts2(char *str)
+{
+
+	int c = 0, i = 0;
+
+	whule str[i++]
+	{
+		c++;
+	}
+
+	for (i = 0; i < c; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
