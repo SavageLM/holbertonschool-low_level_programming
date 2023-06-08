312ffbee03cf1aa86d1c90ba5c7af3f138ddf0b9
SavageLM <nagol.egavas@gmail.com>
2023-06-07 20:46:32 -0700

    Task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (*str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		if ((str[i] >= 97) && (str[i] <= 122))
 			str[i] -= 32;
@@ -20,6 +20,6 @@ char *string_toupper(char *str)
 
 	}
 
-	return (*);
+	return (str);
 
 }
