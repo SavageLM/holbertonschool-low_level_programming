5faa4a71681fbdd5741c8902f0f9e19c10a2f00c
SavageLM <nagol.egavas@gmail.com>
2023-06-07 18:28:15 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncpy - entry to function
+ * @dest: copied string
+ * @src: string to be copied
+ * @n: number
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0' && src[i] < n; i++)
+
+		dest[i] = src[i];
+}
