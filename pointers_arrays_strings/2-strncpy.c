4ca1840cf4a77e45f5fc768f8a79df8b53df21ec
SavageLM <nagol.egavas@gmail.com>
2023-06-07 21:06:32 -0700

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 
 		if (i > n)
 
 			break;
+		i++;
 	}
 
 	dest[i] = '\0';
