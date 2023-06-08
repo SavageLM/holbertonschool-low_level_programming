bbecbb7c352c6c27eec9df9708dd283a34c04b00
SavageLM <nagol.egavas@gmail.com>
2023-06-07 20:31:43 -0700

    Task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reverse_array - entry to function
+ * @a: array to be reversed
+ * @n: number of elements of array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp i;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i]= a[i];
+		a[i] = temp;
+	}
+
+}
