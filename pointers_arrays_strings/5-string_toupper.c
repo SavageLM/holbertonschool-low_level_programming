8c3ea7453caacfa082e388344bc11c29ccc973eb
SavageLM <nagol.egavas@gmail.com>
2023-06-07 20:42:43 -0700

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *string_toupper - entry to function
+ * @*: string to be checked
+ * Return: returns changed string
+ */
+
+char *string_toupper(char *);
+{
+	int i = 0
+
+	while (*[i] != '\0')
+	{
+		if ((*[i] >= 97) && (*[i] <= 122))
+			*[i] -= 32;
+
+		i++;
+
+	}
+
+	return (*);
+
+}
