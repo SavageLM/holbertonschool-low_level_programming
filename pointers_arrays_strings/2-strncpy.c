a33e381bcb9d7643add5716abea4f175f08a4c05
SavageLM <nagol.egavas@gmail.com>
2023-06-07 18:30:37 -0700

    Task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -6,6 +6,7 @@
  * @dest: copied string
  * @src: string to be copied
  * @n: number
+ * Return: destination of copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -15,4 +16,6 @@ char *_strncpy(char *dest, char *src, int n)
 	for (i = 0; src[i] != '\0' && src[i] < n; i++)
 
 		dest[i] = src[i];
+
+	return (dest);
 }
