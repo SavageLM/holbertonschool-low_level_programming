9102c208cfb867f9a32199365d3a16331364881a
SavageLM <nagol.egavas@gmail.com>
2023-06-09 11:19:36 -0700

    Task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *leet - entry to function
+ * @str: string to be checked
+ * Return: returns changed string
+ */
+
+char *leet(char *)
+{
+	int i = 0, c = 0;
+
+	char A1[] = {97,101,111,116,108,65,69,79,84,76};
+	char A2[] = {52,51,48,55,49,52,51,48,55,49};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (c = 0; A1[c] !='\0'; c++)
+			{
+				if (str[i] == A1[c])
+					str[i] = A2[c];
+			}
+	}
+
+	return (str);
+
+}
