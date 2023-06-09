029d024254d42857a66aa4efef506b0477083351
SavageLM <nagol.egavas@gmail.com>
2023-06-09 15:35:01 -0700

    Task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,14 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	char *p;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			*p = s[i];
-			return (*p);
+			return (s[i]);
 		}
 	}
 
