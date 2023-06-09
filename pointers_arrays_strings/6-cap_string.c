31ff50fe632e454a12887f723a944d89a756d0c7
SavageLM <nagol.egavas@gmail.com>
2023-06-09 09:47:23 -0700

    Task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *cap_string - entry to function
+ * @str: string to be checked
+ * Return: returns changed string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, m = 0;
+
+	char s[] = ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', " ";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 97) && (str[i] <= 122))
+		{
+			for (m = 0; m < 13; m++)
+			{
+				if str[i - 1] == s[m]
+					str[i] = str[1] - 32;
+			}
+		}
+	}
+
+	return (str);
+
+}
