b6af71ec2ff5779a5de1181cfd9a65e5360e36ed
SavageLM <nagol.egavas@gmail.com>
2023-06-12 12:49:05 -0700

    Task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -18,7 +18,11 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				c++;
+				break;
+			}
+
 			if (accept[j] == '\0')
 				return (c);
 		}
