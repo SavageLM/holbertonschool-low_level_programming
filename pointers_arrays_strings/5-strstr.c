f1e19f6ff896ef1171a59946bb6deafa2d0d916d
SavageLM <nagol.egavas@gmail.com>
2023-06-12 10:52:29 -0700

    Task 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0, c = 0;
 
-	if (needle[i] =='\0')
+	if (needle[i] == '\0')
 		return (&haystack[c]);
 
 	for (i = 0; haystack[i] != '\0'; i++)
