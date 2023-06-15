a699e231d76307efe088448cf8d1d083828e5131
SavageLM <nagol.egavas@gmail.com>
2023-06-15 11:20:05 -0700

    ask 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion- entrance to the function 
+ * @s: String to be printed
+ * 
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+
+	else
+		_putchar('\n')
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+This README is for the recursion project. All files contained in this directory are for the holberton school.
