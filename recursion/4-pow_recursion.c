0a0cb8609126c067f9f7260203ed1b92685c4ee3
SavageLM <nagol.egavas@gmail.com>
2023-06-15 12:36:38 -0700

    Task 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion- entry to funtcion
+ * @x: interget to raised
+ * @y: exponent power
+ * Return: value of x rasied to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int exp = x;
+
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+	
+	exp *= -pow_recursion(x, y -1);
+
+	return (exp);
+}
