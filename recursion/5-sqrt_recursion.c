5191c637e11eb972e71bc67bf0b9de03bbf96271
SavageLM <nagol.egavas@gmail.com>
2023-06-16 10:44:54 -0700

    Task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,10 +4,11 @@
 /**
  * _sqrt_recursion- entry to function
  * @n: number to square
+ * @root: numbers to check
  * Return: returns the natural square or -1
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int root)
 {
 
 	int root = 0, check = root * root;
