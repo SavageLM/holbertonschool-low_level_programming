70533d199e36335daebe4c23df1de29e442873d5
SavageLM <nagol.egavas@gmail.com>
2023-06-17 23:40:10 -0700

    Task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- entry to function
+ * @argc: numb er of arguemnets passed
+ * @argv: array of arguements
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)) argv)
+{
+	printf("%d\n", argc);
+}
