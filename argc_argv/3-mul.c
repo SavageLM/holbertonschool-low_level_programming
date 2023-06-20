64ceb39590bbc8a91054bc5670c99b3dd607d771
SavageLM <nagol.egavas@gmail.com>
2023-06-20 10:54:59 -0700

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- entry to function
+ * @argc: numb er of arguemnets passed
+ * @argv: array of arguements
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int fact1, fact2,m prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	fact1 = atoi(argv[1]);
+	fact2 = atoi(argv[2]);
+	prod = fact1 * fact2;
+
+	printf("%d\n", prod);
+
+	return (0);
+}
