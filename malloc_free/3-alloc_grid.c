2631250bbcdb2c49f22e8da30385391d7390b223
SavageLM <nagol.egavas@gmail.com>
2023-06-23 12:44:35 -0700

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
 
-	multarr = malloc(sizeof(int) * width * height);
+	multarr = malloc(sizeof(int *) * height);
 
 	if (multarr == NULL)
 		return (NULL);
