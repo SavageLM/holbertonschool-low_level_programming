a2af244ed9d60550d3f96a22619f1505b45b40c4
SavageLM <nagol.egavas@gmail.com>
2023-06-23 12:42:26 -0700

    Task 2

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	if (multarr == NULL)
 		return (NULL);
 
-	for (i = 0; i < height, i++)
+	for (i = 0; i < height; i++)
 	{
 		multarr[i] = malloc(sizeof(int) * width);
 
