7460bff568d5c2f5d99cccfff438c992873c3cbc
SavageLM <nagol.egavas@gmail.com>
2023-06-24 21:24:41 -0700

    Task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,12 @@
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *mem;
+	void *mem;
 
 	mem = malloc(sizeof(unsigned int) * b);
 
 	if (mem == NULL)
-		return (98);
+		exit (98);
 
 	return (mem);
 }
