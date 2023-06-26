abbd00b6442693cb471ad2e73b99b8672fb4d91f
SavageLM <nagol.egavas@gmail.com>
2023-06-26 09:59:06 -0700

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *nconcat;
-	unsigned int i, j, len1;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -22,9 +22,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
+	for (i = 0; s2[i] && i < n; i++)
+		len2++;
+
 	len1 = strlen(s1);
 
-	nconcat = malloc(sizeof(char) *(len1 + 1));
+	nconcat = malloc(sizeof(char) * (len1 + len2));
 
 	if (nconcat == NULL)
 		return (NULL);
