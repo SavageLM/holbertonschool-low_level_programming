a007fa3a00e83b1a918c246f89ff9ad2530aa678
SavageLM <nagol.egavas@gmail.com>
2023-06-30 16:00:42 -0700

    Task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,10 +22,8 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (s == ops[i].op)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
+
+	return (ops[i].f);
 }
