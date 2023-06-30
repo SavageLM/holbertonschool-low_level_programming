3056a9fd37d9808d05530c68e1540b7762855e32
SavageLM <nagol.egavas@gmail.com>
2023-06-30 12:33:18 -0700

    Task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func- function to determine function used for math
+ * @s: arguement passed to function
+ * Return: pointer to correct math operation function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	
+	int i;
+
+	for ( i = 0; ops[i]; i++)
+	{
+		if (s == ops[i])
+			return (ops[i].f);
+	}
+
+	return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * op_add- function to add two integers
+ * @a: an integer
+ * @b: an integer
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- function to subtract two integers
+ * @a: an integer
+ * @b: an integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- function to multiply two integers
+ * @a: an integer
+ * @b: an integer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- function to divide two integers
+ * @a: an integer
+ * @b: an integer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- function to get remainder of two integers
+ * @a: an integer
+ * @b: an integer
+ */
+
+int op_mod(int a, int b)
+{
+	return ( a % b);
+}
