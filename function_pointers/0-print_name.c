d3d8f89efbaeb21bc115a535069b4cb38bd78a3a
SavageLM <nagol.egavas@gmail.com>
2023-06-29 19:35:31 -0700

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_name- function that calls on function f to print a name
+ * @name: name to print
+ * @(*f): function that prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if ((name == NULL) || (f == NULL))
+		return;
+
+	f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,1 @@
+This README is for the Pointer to functions projects at Holberton
