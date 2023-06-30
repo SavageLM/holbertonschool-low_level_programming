a942498f625c411b472af44a73a207bb6d942772
SavageLM <nagol.egavas@gmail.com>
2023-06-29 18:32:40 -0700

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * free_dog- function to free dog
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
