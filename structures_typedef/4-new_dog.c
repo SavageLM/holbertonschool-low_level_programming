10e2170372ed6f46a5c1b97d0beb94ec75e5d897
SavageLM <nagol.egavas@gmail.com>
2023-06-29 18:20:45 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *tmpName, *tmpOwner;
+	char *tmpName = malloc(sizeof(char) * name), *tmpOwner = malloc(sizeof(char) * owner);
 	int i, j;
 
 	for (i = 0; name[i] != '\0'; i++)
