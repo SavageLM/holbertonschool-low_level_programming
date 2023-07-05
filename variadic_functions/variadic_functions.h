a907bc60cbb737b45f34368329e63b9814de8962
SavageLM <nagol.egavas@gmail.com>
2023-07-05 09:52:21 -0700

    Task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,11 @@ typedef struct checker
 	void (*print)(va_list arg, char *separator);
 }checker_t;
 
+void print_char(va_list arg, char *separator);
+void print_int(va_list arg, char *separator);
+void print_float(va_list arg, char *separator);
+void print_string(va_list arg, char *separator);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
