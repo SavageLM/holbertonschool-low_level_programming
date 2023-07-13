5df2899cc5a47e65e233a5fc4a016eccb9c04aa9
SavageLM <nagol.egavas@gmail.com>
2023-07-13 16:24:48 -0700

    Added 3-add_node_end file and code

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node to the end of a linked list
+ * @head: start of list
+ * @str: string to be duplicated
+ * Return: returns address of new node or NULL
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newend, *oldend;
+	int i;
+
+	newend = malloc(sizeof(list_t));
+
+	if ((newend == NULL) || (str == NULL))
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	newend->str = strdup(str);
+	newend->len = i;
+	newend->next = NULL;
+
+	if (*head == NULL)
+		*head = newend;
+	else
+	{
+		oldend = *head;
+		while (oldend->next != NULL)
+			oldend = oldend->next;
+		oldend->next = newend;
+	}
+
+	return (newend);
+}
+
+
