22cbe74aa752599766d40f36d34dd4631cfedf3b
SavageLM <nagol.egavas@gmail.com>
2023-07-13 15:58:45 -0700

    Added a NULL check for str

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a node to a list
+ * @head: pointer to beginning of list
+ * @str: string to duplicated and stored
+ * Return: Returns address of new node, or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	list_t *newlist;
+
+	newlist = malloc(sizeof(list_t));
+	
+	if ((newlist == NULL) || (str == NULL))
+		return (NULL);
+
+	while(str[i])
+		i++;
+
+	newlist->str = strdup(str);
+	newlist->len = i;
+	newlist->next = *head;
+
+	*head = newlist;
+
+	return (newlist);
+}
