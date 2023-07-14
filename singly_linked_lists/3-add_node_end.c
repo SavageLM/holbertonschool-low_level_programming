aaae985f3a60eb5e0b66870ac254f23b3d1425c0
SavageLM <nagol.egavas@gmail.com>
2023-07-13 18:29:44 -0700

    Changed loop in 3-add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if ((newend == NULL) || (str == NULL))
 		return (NULL);
 
-	while (str[i])
+	for (i = o; str[i];)
 		i++;
 
 	newend->str = strdup(str);
