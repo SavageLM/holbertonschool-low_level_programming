52db76cb07ab80df23bca65274498129dfb0414e
SavageLM <nagol.egavas@gmail.com>
2023-07-13 18:15:00 -0700

    MOved struct location in header

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -7,15 +7,6 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-/*Prototypes*/
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-int _putchar(char c);
-
 /**
  * struct list_s - a singly linked list
  * @str: a pointer to a malloc'd string
@@ -31,5 +22,14 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+/*Prototypes*/
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+int _putchar(char c);
 
 #endif
