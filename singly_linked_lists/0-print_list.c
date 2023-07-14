f8a0ad7576ed88eaacb03c30b7df3fefdadbe476
SavageLM <nagol.egavas@gmail.com>
2023-07-13 18:16:16 -0700

    Changed commas to semi colons in file0-print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@ size_t print_list(const list_t *h)
 {
 	size_t numnode;
 
-	for (numnode = 0, h != NULL, numnode++)
+	for (numnode = 0; h != NULL; numnode++)
 	{
 		if (h->str ==NULL)
 			printf("[0] (nil)\n");
