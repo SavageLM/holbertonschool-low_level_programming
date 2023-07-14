3da4e5b2072557bd27de28fe47a8c48d1d1643da
SavageLM <nagol.egavas@gmail.com>
2023-07-14 12:13:54 -0700

    Corrected syntax errors

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,12 +8,12 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, converter = 1;
+	unsigned int i, converter = 1, exponet = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i] != NULL; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if ((b[i] != 0) || (b[i] != 1))
 			return (0);
