93fa84e2e574f25d11cb46fd33ca6e5daddee3b3
SavageLM <nagol.egavas@gmail.com>
2023-07-17 16:11:27 -0700

    Corrected spelling mistake

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (-1);
 
-	for (len = 0; text_context[len];)
+	for (len = 0; text_content[len];)
 		len++;
 
 	tmp = open(filename, O_RDWR | O_CREAT | O_TRUNC);
