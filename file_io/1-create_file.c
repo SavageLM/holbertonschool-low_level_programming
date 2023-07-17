caa38b07658c0a6096eea54c0902a09fdbfedddf
SavageLM <nagol.egavas@gmail.com>
2023-07-17 16:15:47 -0700

    Corrected betty, moved conditional, added flags to open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,20 +13,19 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
 
 	for (len = 0; text_content[len];)
 		len++;
 
-	tmp = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	tmp = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (tmp == -1)
 		return (-1);
 
-	write(tmp, text_content, len);
+	if (text_content != NULL)
+		write(tmp, text_content, len);
 
 	close(tmp);
-	return(1);
+	return (1);
 }
 
