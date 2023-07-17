603ced00b3d9c67f20d40721ee63c5d22230c90d
SavageLM <nagol.egavas@gmail.com>
2023-07-17 10:32:10 -0700

    Added 2-get_bit file and code

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - Function to retrieve value of bit
+ * @n: binary number to check
+ * @index: index of n, starting at 0
+ * Return: value of bit at index, or -1 on error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (o);
+
+	return (1);
+}
