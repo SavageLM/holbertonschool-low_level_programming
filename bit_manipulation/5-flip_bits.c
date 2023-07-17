64f3a596ff310675c11e0436e8bf0ab05b674f50
SavageLM <nagol.egavas@gmail.com>
2023-07-17 11:14:23 -0700

    Added 5-flip_bits file and code

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - function to flip a given bit
+ * @n: number
+ * @m: number to flip to
+ * Return: number of bits needed to flip n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int check = n^m, bits = 0;
+
+	while (check > 0)
+	{
+		bits += (check & 1);
+		check >>= 1;
+	}
+
+	return (bits);
+}
