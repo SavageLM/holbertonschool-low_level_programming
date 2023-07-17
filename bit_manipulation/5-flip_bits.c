7df418e586ce78cae4d1ceca0ba71f115567c0d4
SavageLM <nagol.egavas@gmail.com>
2023-07-17 11:15:25 -0700

    Correct style

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int check = n^m, bits = 0;
+	unsigned long int check = n ^ m, bits = 0;
 
 	while (check > 0)
 	{
