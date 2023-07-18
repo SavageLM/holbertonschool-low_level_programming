f6739cb13562204c1d5f366f58d2e7bdf98cbab7
SavageLM <nagol.egavas@gmail.com>
2023-07-17 18:19:28 -0700

    assigned variables as pointers

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t NewEnd, OldEnd;
+	listint_t *NewEnd, *OldEnd;
 
 	NewEnd = malloc(sizeof(listint_t));
 	if (NewEnd == NULL)
