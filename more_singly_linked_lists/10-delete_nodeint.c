40c3e9fd917e4728ffd70435779450a5e1a2f13b
SavageLM <nagol.egavas@gmail.com>
2023-07-17 19:38:55 -0700

    Added second variable for holding head data

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -7,9 +7,9 @@
  * Return: 1 on success, or -1 on fail
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+int delete_nodeint_at_index(listint_t **head, u)nsigned int index)
 {
-	listint_t *tmp;
+	listint_t *tmp, *copy = *head;
 	unsigned int counts;
 
 	if (*head == NULL)
@@ -17,17 +17,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		free(copy);
 		return (1);
 	}
 
 	for (counts = 0; counts < (index - 1); counts++)
 	{
-		if (*head->next == NULL)
+		if (copy->next == NULL)
 			return (-1);
-		*head = (*head)->next;
+		copy = copy->next;
 	}
 
-	tmp = (*head)->next;
-	*head = tmp->next;
+	tmp = copy->next;
+	copy = tmp->next;
+	free(tmp);
 	return (1);
 }
