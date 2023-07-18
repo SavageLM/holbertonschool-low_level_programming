8176aadfc4ad7ebdf704bf4eadd9b0d4064ff0f6
SavageLM <nagol.egavas@gmail.com>
2023-07-17 18:46:52 -0700

    Added 7-get_nodeint file and code

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Does what it says
+ * @head: start of list
+ * @index: index of desired node
+ * Return: Returns node at index or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int checker;
+
+	for (checker = 0; checker < index; checker++)
+	{
+		if (head = NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
