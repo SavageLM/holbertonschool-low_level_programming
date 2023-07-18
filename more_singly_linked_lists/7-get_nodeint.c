8976afd69e7627349d0f62019c7bf3b7737a8222
SavageLM <nagol.egavas@gmail.com>
2023-07-17 18:47:48 -0700

    Fixed comparative statement

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (checker = 0; checker < index; checker++)
 	{
-		if (head = NULL)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
