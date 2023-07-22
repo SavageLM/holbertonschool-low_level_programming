3bc6dff938e03aa7496f82ddcaa5cd4e4431fab3
SavageLM <nagol.egavas@gmail.com>
2023-07-21 17:59:25 -0700

    Added7-insert_dnodeint file and code

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - does what it says
+ * @idx: index to insert new node at
+ * @n: value to give new node
+ * @h: pointer to start of list
+ * Return: address of new node or NULL
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *newnode;
+	unsigned int i;
+
+	if (*h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; i < index; i++)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->prev = tmp;
+	newnode->next = tmp->next;
+	tmp->next->prev = newnode;
+	tmp->next = newnode;
+
+	return (newnode);
+}
