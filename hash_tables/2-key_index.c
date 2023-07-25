277dc934f5e5a8edb25616d4483d419a536ccc2e
SavageLM <nagol.egavas@gmail.com>
2023-07-24 17:22:57 -0700

    Added 2-key_index file and code

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_table.h"
+
+/**
+ * key_index - finds the index of a key
+ * @key: key provided
+ * @size: size of hash table
+ * Return: index of key in hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
