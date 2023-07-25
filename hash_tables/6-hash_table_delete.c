37c1e74014192bfad0c6c7bb312fc9de80d94214
SavageLM <nagol.egavas@gmail.com>
2023-07-24 18:56:19 -0700

    Added 6-hash_table_delete file and code

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_delete - does what it says
+ * @ht: table to delete
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *tmp_tbl = ht;
+	hash_node_t *node, *tmp_node;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp_node = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp_node;
+			}
+		}
+	}
+	free(tmp_tbl->array);
+	free(tmp_tbl);
+}
