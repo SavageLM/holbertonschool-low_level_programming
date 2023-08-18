b78b9fe31e9dce3e0ced5888759adc0f72bdd808
SavageLM <nagol.egavas@gmail.com>
2023-08-17 17:07:14 -0700

    Fixed include guard, for real this time

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,3 +8,5 @@
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+#endif
