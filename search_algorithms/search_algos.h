42a870044be7099eaff2c017c775f69f1743b376
SavageLM <nagol.egavas@gmail.com>
2023-08-17 18:21:14 -0700

    Added print prototype to header

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -8,5 +8,6 @@
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_search(int *array, size_t min, size_t max);
 
 #endif
